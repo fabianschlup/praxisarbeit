837ecb7100394f96c55fc808b31fd7f3bcbfc991
fabianschlup <127875012+fabianschlup@users.noreply.github.com>
2023-03-27 16:58:13 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,12 +8,19 @@ from flask_login import LoginManager
 from config import Config
 
 # Übernommen aus den Beispielen von Miguel Grinberg
+# Initialize Flask app
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
+
+# Initialize SQLAlchemy
+db = SQLAlchemy()
+db.init_app(app)
 migrate = Migrate(app, db)
+
+## Initialize LoginManager
 login = LoginManager(app)
-login.login_view = 'login'
+login.login_view = 'auth.login'
+login.login_message = ('Please log in to access this page.')
 
 # Übernommen aus den Beispielen von Miguel Grinberg
 from app import routes, models, errors
